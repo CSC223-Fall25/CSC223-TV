d4d08dc95b1bcc7404770cf2a1ea8edabfa77a96
vutr2 <vutr2@furman.edu>
2025-02-11 20:10:25 -0500

    My lab assignment 2

--- app/src/test/java/csc223/tv/NodeTest.java
+++ app/src/test/java/csc223/tv/NodeTest.java
@@ -1,5 +1,31 @@
 package csc223.tv;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class NodeTest {
     
+
+    @Test
+    public void testNode(){
+        Node node = new Node('A');
+
+        assertEquals(node.data, 'A');
+        assertNull(node.next);
+        assertNull(node.previous);
+    }
+
+    @Test
+    public void testNodeLink(){
+        Node first = new Node('a');
+        Node second = new Node('b');
+
+        first.next = second;
+        second.previous = first;
+
+        assertEquals(first.next.data, 'b');
+        assertEquals(second.previous.data, 'a');
+        assertEquals(first.next, second);
+        assertEquals(second.previous, first);
+    }
 }
