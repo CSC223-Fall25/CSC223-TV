14488ccd0294c88fbb9eb05a1e8eef34d9dba347
vutr2 <vutr2@furman.edu>
2025-04-14 10:33:24 -0400

    Minimum Fuel Cost Problem - Med

--- app/src/main/java/csc223/tv/MinimumFuelCost.java
+++ app/src/main/java/csc223/tv/MinimumFuelCost.java
@@ -3,47 +3,49 @@ package csc223.tv;
  * 2477. Minimum Fuel Cost to Report to the Capital
  * https://leetcode.com/problems/minimum-fuel-cost-to-report-to-the-capital/description/?envType=problem-list-v2&envId=tree
  */
+import java.util.*;
 
-
-class Solution {
+ class Solution {
     // store total fuel cost
     public long ans = 0;
 
     public long minimumFuelCost(int[][] roads, int seats) {
-        int[][] graph = new int[roads.length + 1][roads.length + 1];
-        int[] degree = new int[roads.length + 1];
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        
 
         //building graph 
         for (int[] road : roads){
             int u = road[0];
             int v = road[1];
 
-            // add u to v's neighbor list and increment degree
-            graph[u][degree[u]++] = v;
-            // add v to u's neighbor list and increment degree
-            graph[v][degree[v]++] = u;
+            if (!graph.containsKey(u)){
+                graph.put(u, new ArrayList<>());
+            }
+            graph.get(u).add(v);
+
+            if (!graph.containsKey(v)){
+                graph.put(v, new ArrayList<>());
+            }
+            graph.get(v).add(u);
+    
         }
 
         // call dfs on first node (with no parents)
-        dfs(graph, degree, 0, -1, seats);
+        dfs(graph, 0, -1, seats);
         return ans;
 
     }
 
-    public int dfs(int[][] graph, int[] degree, int u, int prev, int seats){
+    public int dfs(Map<Integer, List<Integer>> graph,  int u, int prev, int seats){
 
         // initialize the people to 1
         int people = 1;
 
         //Traversal to all neighbor of node u
-        for (int i = 0; i < degree[u]; i ++) {
-            int v = graph[u][i];
-            if (v == prev){
-                continue;
-            }
-            // add the subtree to the people
-            people += dfs(graph, degree, v, u, seats);
-
+        for (int v : graph.getOrDefault(u, new ArrayList<>())) {
+            if (v == prev) {
+                continue;}
+            people += dfs(graph, v, u, seats);
         }
         if (u >0){
             // calculate car need for this node to parent using celi
